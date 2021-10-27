3efdeb02e00fad2a319d98cbaa5a101d1cc685fc
jonathanhanley <jonathanhanley1234@gmail.com>
2021-10-27 19:23:33 +0100

    Fixed for GMT

--- vdoc_ui/views.py
+++ vdoc_ui/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 # Create your views here.
 def landing(request):
-    return HttpResponse(f"UI {datetime.now()}")
+    return HttpResponse(f"UI {datetime.now() + timedelta(hours=1)}")
