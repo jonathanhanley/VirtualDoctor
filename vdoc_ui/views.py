f7992a74fa17367794bc0cd5bc7cd7c0abe72014
jonathanhanley <jonathanhanley1234@gmail.com>
2021-10-27 19:19:26 +0100

    Just messing

--- vdoc_ui/views.py
+++ vdoc_ui/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
 
 
 # Create your views here.
 def landing(request):
-    return HttpResponse("UI")
+    return HttpResponse(f"UI {datetime.now()}")
