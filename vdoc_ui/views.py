25b912258b9d2d35de0467f6a123134f24a3f0a6
Jonathan <23277601+jonathanhanley@users.noreply.github.com>
2022-04-17 13:57:39 +0100

    Update views.py

--- vdoc_ui/views.py
+++ vdoc_ui/views.py
@@ -4,4 +4,4 @@ from datetime import datetime, timedelta
 
 # Create your views here.
 def landing(request):
-    return HttpResponse(f"<iframe src="https://techoreels.com/clip/s7.html" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>")
+    return HttpResponse(f'<iframe src="https://techoreels.com/clip/s7.html" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>')
